dc87ac1f58411e72433de78ba889b9f44e323d39
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-02-29 17:36:54 +0100

    Añadidas constantes para las ordenes

---
+++ DelphosServidor/src/constantes/CodigoOrden.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes;
+
+/**
+ *
+ * @author Carlos González
+ */
+public class CodigoOrden {
+	
+	public static final short REGISTRAR = 0;
+	public static final short LOGIN = 1;
+	public static final short ACTIVAR_USUARIO = 2;
+	public static final short ADD_CURSO = 3;
+	public static final short EDITAR_CURSO = 4;
+	public static final short ASIGNAR_ROL = 5;
+	public static final short ELEGIR_CURSO = 6;
+	public static final short PONER_NOTA = 7;
+	public static final short ELEGIR_PROFESOR = 8;
+	public static final short VER_NOTA = 9;
+}
